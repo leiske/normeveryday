ed04fe02f20a8b8f7aaa85683a2025198ce60a22
Colby <colby.leiske@gmail.com>
2022-06-25 23:26:46 -0700

    cleanup decrypt and add encrypt

--- utils/decrypt_secret.sh
+++ utils/decrypt_secret.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # https://docs.github.com/en/actions/security-guides/encrypted-secrets#storing-large-secrets
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$JOKES_PASSWORD" \
